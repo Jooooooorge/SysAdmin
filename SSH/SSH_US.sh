14d735f0240288d10dc3a41a07344d2c467273d0
Jooooooorge <jorgeaguilaruni@gmail.com>
2025-02-26 12:11:15 -0700

    Update SSH_US.sh

--- SSH/SSH_US.sh
+++ SSH/SSH_US.sh
@@ -1,23 +1,15 @@
 #********************************************************************************************************************************
 # ACTIVIDAD 4
 # Script para automatizar la creación de un servidor SSH
+# Importar funciones
+source US.sh
 
 # Asegurarnos de que el sistema esta actualizado
 sudo apt update
 sudo apt install openssh-server -y
 
-
-# Configuración de la red estatica 
-echo "network:
-  version: 2
-  renderer: networkd
-  ethernets:
-    enp0s3:
-      dhcp4: false
-      addresses:
-        - 192.168.1.130/24" | sudo tee /etc/netplan/00-installer-config.yaml > /dev/null
-# Aplicar cambios de red
-sudo netplan apply  
+# Llamada a la función para configurar la ip estatica
+StaticIpConfig
 
 # Habilitamos ssh
 sudo systemctl enable ssh
