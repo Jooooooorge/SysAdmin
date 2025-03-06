9659bcbb32c2df6e0263d38c6eaa33ce05d5ca47
Jooooooorge <jorgeaguilaruni@gmail.com>
2025-03-06 13:33:17 -0700

    Update Prueba.ps1

--- FTP/Prueba.ps1
+++ FTP/Prueba.ps1
@@ -112,12 +112,6 @@ while ($true) {
                 # Crear el usuario
                 New-LocalUser -Name $Usuario -Password $Contra -FullName $Usuario -ErrorAction Stop
 
-                # Configurar que la contraseña nunca expire
-                Set-LocalUser -Name $Usuario 
-
-                # Configurar que la cuenta nunca expire
-                Set-LocalUser -Name $Usuario 
-
                 # Asignar el usuario al grupo
                 Add-LocalGroupMember -Group $Grupo -Member $Usuario -ErrorAction Stop
 
