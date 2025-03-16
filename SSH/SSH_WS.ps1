da66319c6219c216faf0bea4500e167a9222ff4a
Jooooooorge <jorgeaguilaruni@gmail.com>
2025-03-15 23:34:52 -0700

    *>$null

--- SSH/SSH_WS.ps1
+++ SSH/SSH_WS.ps1
@@ -10,16 +10,16 @@ ConfigurarIpEstatica
 $sshFeature = Get-WindowsCapability -Online | Where-Object Name -like 'OpenSSH.Server*'
 
 if ($sshFeature.State -ne 'Installed') {
-    Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0 -ErrorAction SilentlyContinue
+    Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0 -ErrorAction SilentlyContinue *>$null
 }
 
 # Iniciar el servicio SSHD
-Start-Service sshd -ErrorAction SilentlyContinue
-Set-Service -Name sshd -StartupType Automatic -ErrorAction SilentlyContinue
+Start-Service sshd -ErrorAction SilentlyContinue *>$null
+Set-Service -Name sshd -StartupType Automatic -ErrorAction SilentlyContinue *>$null
 
 # Configurar firewall para permitir SSH
-if (-not (Get-NetFirewallRule -Name 'OpenSSH-Server' -ErrorAction SilentlyContinue)) {
-    New-NetFirewallRule -Name 'OpenSSH-Server' -DisplayName 'OpenSSH Server' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+if (-not (Get-NetFirewallRule -Name 'OpenSSH-Server' -ErrorAction SilentlyContinue *>$null)) {
+    New-NetFirewallRule -Name 'OpenSSH-Server' -DisplayName 'OpenSSH Server' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22 *>$null
 }
 Write-Host "SSH Instalado y configurado correctamente"
 
